0a181083549ba595d67ddce3c4e11c9d4997a704
ardoooo <naumov.av@phystech.edu>
2024-05-12 20:58:25 +0000

    updated task1: added cronjob for script

--- task1/script/script.py
+++ task1/script/script.py
@@ -4,6 +4,11 @@ from datetime import datetime
 import time
 
 url = os.getenv('SERVICE_URL') + '/statistics'
+sleep_per_s = int(os.getenv('INNER_INTERVAL_S'))
+
+log_directory = 'logs'
+os.makedirs(log_directory, exist_ok=True)
+log_file_path = os.path.join(log_directory, 'statistics.log')
 
 while True:
     response = requests.get(url)
@@ -12,7 +17,7 @@ while True:
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_entry = f"{current_time}: {count}\n"
 
-    with open('statistics.log', 'a') as file:
+    with open(log_file_path, 'a') as file:
         file.write(log_entry)
 
-    time.sleep(5)
+    time.sleep(sleep_per_s)
